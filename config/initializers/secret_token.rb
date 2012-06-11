aec1e7f56d1428e052d406a178ee2625778a80e2
Len Smith <len@barrison.com>
2012-06-11 12:53:28 -0400

    Iniital Commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paleopapers::Application.config.secret_token = '8cd3b1dac15554151702a49e8977a1d178ca702924ae9dd616a75a6c10c0b132b5887adf02cceaf08e931913d52a18a331c73d1b32fe04d8632a7a024d7d3b9c'
